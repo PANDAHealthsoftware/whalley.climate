0296763dda528105ba985372e34b59fc261cbf67
PANDAHealthsoftware <roberts1979simon@gmail.com>
2025-05-09 09:45:16 +0100

    removed unused

--- WhalleyClimate.Web/Services/IImageService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WhalleyClimate.Web.Services;
-
-public interface IImageService
-{
-    Task<string> UploadImageAsync(IFormFile file);
-    Task<bool> DeleteImageAsync(string relativePath);
-}
